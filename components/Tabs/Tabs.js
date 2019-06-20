853686a44c4062ad33b01c94422b83cbf00ac7c0
Kenya-a <Kenya-a@users.noreply.github.com>
2019-06-20 17:51:36 -0500

    still a bug in the tabs.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,7 +8,7 @@ class TabLink {
     this.data = this.element.dataset.tab;
 
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(`.tabs-item[data-tab = '${this.data}']`);
+    this.itemElement = document.querySelector(`.tabs-item[data-tab = '${this.data}]`);
 
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
@@ -25,7 +25,11 @@ class TabLink {
     const links = document.querySelectorAll('.tab-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(item => item.classList.remove('tabs-link-selected'));
+    Array.from(links).forEach((links) => this.element.classList.remove('tabs-link-selected'));
+
+    // links.forEach(link => {
+    //   link.classList.remove('tabs-link-selected')
+    // });
 
     // Add a class named "tabs-link-selected" to this link
     // // ??Where was the link created??
